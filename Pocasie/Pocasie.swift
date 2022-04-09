26e739a678528dfce0d8c13ceb592f84d67331b4
Pavel Surovy <surolm@centrum.sk>
2022-04-09 07:39:44 +0200

    weather struct for decoding

--- Pocasie/Pocasie.swift
+++ Pocasie/Pocasie.swift
@@ -10,4 +10,32 @@ import Foundation
 struct WeatherMain: Codable {
     let lat: Double
     let lon: Double
+    let current: WeatherCurrent
+    let daily: [WeatherDaily]
+    
+    struct WeatherCurrent: Codable {
+        let temp: Double
+        let feels_like: Double
+        let weather: [WeatherDescription]
+        
+        struct WeatherDescription: Codable {
+            let main: String
+            let icon: String
+        }
+    }
+    
+    struct WeatherDaily: Codable {
+        let dt: Int
+        let temp: DailyTemp
+        let weather: [DailyDescription]
+        
+        struct DailyTemp: Codable {
+            let day: Double
+        }
+        
+        struct DailyDescription: Codable {
+            let main: String
+            let icon: String
+        }
+    }
 }
