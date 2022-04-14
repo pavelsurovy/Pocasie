081c073506ea71c2bd7541e386c06e70659dcee1
Pavel Surovy <surolm@centrum.sk>
2022-04-14 19:11:47 +0200

    DetailView refactoring

--- Pocasie/View/DetailView.swift
+++ Pocasie/View/DetailView.swift
@@ -8,40 +8,9 @@
 import SwiftUI
 
 struct DetailView: View {
+    @StateObject private var detailModel = DetailViewModel()
+    
     var lokalita: Lokality
-    
-    @State private var weatherResult: WeatherMain?
-    
-    let icon = [
-        "01d": "sun.max.fill", // clear sky
-        "02d": "cloud.sun.fill", // few clouds
-        "03d": "cloud.fill", // scattered clouds
-        "04d": "smoke.fill", // broken clouds
-        "09d": "cloud.sun.rain.fill", // shower rain
-        "10d": "cloud.heavyrain.fill", // rain
-        "11d": "cloud.bolt.fill", // thunderstorm
-        "13d": "cloud.snow.fill", // snow
-        "50d": "text.aligncenter" // mist
-    ]
-    
-    var svkDescription: String {
-        switch weatherResult?.current.weather.first!.main {
-        case "Clear":
-            return "Jasno"
-        case "Clouds":
-            return "Oblačno"
-        case "Thunderstorm":
-            return "Búrka"
-        case "Drizzle":
-            return "Mrholenie"
-        case "Rain":
-            return "Dážď"
-        case "Snow":
-            return "Sneženie"
-        default:
-            return "..."
-        }
-    }
     
     var body: some View {
 
@@ -51,29 +20,29 @@ struct DetailView: View {
                     .font(.callout)
                     .padding(.bottom, 20)
                     
-                Text(svkDescription)
+                Text(detailModel.svkDescription)
                     .font(.system(size: 35))
                     .padding(.bottom, -30)
                 
-                Text("\(Int(weatherResult?.current.temp ?? 0))°C")
+                Text("\(Int(detailModel.weatherResult?.current.temp ?? 0))°C")
                     .font(.system(size: 65))
                     .fontWeight(.black)
                 
-                Text("Pocitová teplota \(Int(weatherResult?.current.feels_like ?? 0))°C")
+                Text("Pocitová teplota \(Int(detailModel.weatherResult?.current.feels_like ?? 0))°C")
                     .padding(.bottom, 50)
                     .padding(.top, -40)
                     .foregroundColor(.gray)
                     
-                if let day = weatherResult?.daily {
+                if let day = detailModel.weatherResult?.daily {
                     ForEach(day, id: \.dt) { day in
                         
                         HStack {
-                            Text("\(denTyzdna(day.dt))")
+                            Text("\(detailModel.denTyzdna(day.dt))")
                                 .frame(width: 110, alignment: .leading)
                             
                             Spacer()
                             
-                            Image(systemName: icon[day.weather.first!.icon] ?? "exclamationmark.square")
+                            Image(systemName: detailModel.icon[day.weather.first!.icon] ?? "exclamationmark.square")
                                 .symbolRenderingMode(.multicolor)
                                 
                             Spacer()
@@ -91,42 +60,8 @@ struct DetailView: View {
         }
         .navigationTitle(lokalita.name)
         .onAppear {
-            stiahniData(lat: lokalita.latitude, lon: lokalita.longitude)
+            detailModel.stiahniData(lat: lokalita.latitude, lon: lokalita.longitude)
         }
-    }
-    
-    func stiahniData(lat: Double, lon: Double) {
-        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&appid=\(APIKey.appID)&exclude=minutely,hourly,alerts&units=metric"
-
-        let url = URL(string: urlString)!
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let data = data else {
-                print("Data Error!")
-                return
-            }
-            
-            if let json = try? JSONDecoder().decode(WeatherMain.self, from: data) {
-                weatherResult = json
-            }
-        }
-        task.resume()
-        
-    }
-    
-    func denTyzdna(_ num: Int) -> String {
-        let formatter = DateFormatter()
-        formatter.setLocalizedDateFormatFromTemplate("EEEE")
-        let jsonDate = Date(timeIntervalSince1970: TimeInterval(num))
-        let dateString = formatter.string(from: jsonDate).capitalized
-        
-        return dateString
     }
 }
 
--- Pocasie/ViewModel/DetailViewModel.swift
+++ Pocasie/ViewModel/DetailViewModel.swift
@@ -7,39 +7,72 @@
 
 import Foundation
 
-//class DetailViewModel: ObservableObject {
-//    
-//    @Published var weatherResult: WeatherMain?
-//    
-//    let icon = [
-//        "01d": "sun.max.fill", // clear sky
-//        "02d": "cloud.sun.fill", // few clouds
-//        "03d": "cloud.fill", // scattered clouds
-//        "04d": "smoke.fill", // broken clouds
-//        "09d": "cloud.sun.rain.fill", // shower rain
-//        "10d": "cloud.heavyrain.fill", // rain
-//        "11d": "cloud.bolt.fill", // thunderstorm
-//        "13d": "cloud.snow.fill", // snow
-//        "50d": "text.aligncenter" // mist
-//    ]
-//    
-//    var svkDescription: String {
-//        switch weatherResult?.current.weather.first!.main {
-//        case "Clear":
-//            return "Jasno"
-//        case "Clouds":
-//            return "Oblačno"
-//        case "Thunderstorm":
-//            return "Búrka"
-//        case "Drizzle":
-//            return "Mrholenie"
-//        case "Rain":
-//            return "Dážď"
-//        case "Snow":
-//            return "Sneženie"
-//        default:
-//            return "..."
-//        }
-//    }
-//    
-//}
+class DetailViewModel: ObservableObject {
+
+    @Published var weatherResult: WeatherMain?
+
+    let icon = [
+        "01d": "sun.max.fill", // clear sky
+        "02d": "cloud.sun.fill", // few clouds
+        "03d": "cloud.fill", // scattered clouds
+        "04d": "smoke.fill", // broken clouds
+        "09d": "cloud.sun.rain.fill", // shower rain
+        "10d": "cloud.heavyrain.fill", // rain
+        "11d": "cloud.bolt.fill", // thunderstorm
+        "13d": "cloud.snow.fill", // snow
+        "50d": "text.aligncenter" // mist
+    ]
+
+    var svkDescription: String {
+        switch weatherResult?.current.weather.first!.main {
+        case "Clear":
+            return "Jasno"
+        case "Clouds":
+            return "Oblačno"
+        case "Thunderstorm":
+            return "Búrka"
+        case "Drizzle":
+            return "Mrholenie"
+        case "Rain":
+            return "Dážď"
+        case "Snow":
+            return "Sneženie"
+        default:
+            return "..."
+        }
+    }
+
+    func stiahniData(lat: Double, lon: Double) {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&appid=\(APIKey.appID)&exclude=minutely,hourly,alerts&units=metric"
+
+        let url = URL(string: urlString)!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("Data Error!")
+                return
+            }
+            
+            if let json = try? JSONDecoder().decode(WeatherMain.self, from: data) {
+                self.weatherResult = json
+            }
+        }
+        task.resume()
+        
+    }
+    
+    func denTyzdna(_ num: Int) -> String {
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("EEEE")
+        let jsonDate = Date(timeIntervalSince1970: TimeInterval(num))
+        let dateString = formatter.string(from: jsonDate).capitalized
+        
+        return dateString
+    }
+}
