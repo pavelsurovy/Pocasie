a5f8945995422b2a4eb8bd362fc2d5ee80eab295
Pavel Surový <64226055+pavelsurovy@users.noreply.github.com>
2024-09-12 20:18:40 +0200

    update for visualcrossing.com

--- Pocasie/View/DetailView.swift
+++ Pocasie/View/DetailView.swift
@@ -8,47 +8,66 @@
 import SwiftUI
 
 struct DetailView: View {
+    
     @StateObject private var detailModel = DetailViewModel()
-    
-    var lokalita: Lokality
-    
-    var body: some View {
 
+    var lokalita: Lokality
+            
+    var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
-                Text("\(Date.now, format: .dateTime.day().month().year())")
-                    .font(.callout)
-                    .padding(.bottom, 20)
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(Date.now, format: .dateTime.day().month().year())")
+                            .font(.callout)
+                            .padding(.bottom, 20)
+                        Text(detailModel.weatherResult?.currentConditions.icon ?? "")
+                        Text(detailModel.svkDescription)
+                            .font(.system(size: 35))
+                            .padding(.bottom, -30)
+                        
+                        Text("\(Int(detailModel.weatherResult?.currentConditions.temp ?? 0))°C")
+                            .font(.system(size: 65))
+                            .fontWeight(.black)
+                        
+                        Text("Pocitová teplota \(Int(detailModel.weatherResult?.currentConditions.feelslike ?? 0))°C")
+                                            .padding(.bottom, 50)
+                                            .padding(.top, -40)
+                                            .foregroundColor(.gray)
+                    }
                     
-                Text(detailModel.svkDescription)
-                    .font(.system(size: 35))
-                    .padding(.bottom, -30)
-                
-                Text("\(Int(detailModel.weatherResult?.current.temp ?? 0))°C")
-                    .font(.system(size: 65))
-                    .fontWeight(.black)
-                
-                Text("Pocitová teplota \(Int(detailModel.weatherResult?.current.feels_like ?? 0))°C")
-                    .padding(.bottom, 50)
-                    .padding(.top, -40)
-                    .foregroundColor(.gray)
+                    Spacer()
                     
-                if let day = detailModel.weatherResult?.daily {
-                    ForEach(day, id: \.dt) { day in
+                    
+                    if let unwrapObrazok = detailModel.weatherResult?.currentConditions.icon {
+                        
+                        Image(systemName: detailModel.icon[unwrapObrazok] ?? "exclamationmark.square")
+                            .resizable()
+                            .symbolRenderingMode(.multicolor)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 80)
+                            .padding(.trailing, 20)
+                        
+                    }
+                }
+                
+                if let day = detailModel.weatherResult?.days {
+                    
+                    ForEach(day, id: \.datetimeEpoch) { day in
                         
                         HStack {
-                            Text("\(detailModel.denTyzdna(day.dt))")
-                                .frame(width: 110, alignment: .leading)
+                            Text("\(detailModel.denTyzdna(day.datetimeEpoch))")
+                                .frame(width: 100, alignment: .leading)
                             
                             Spacer()
                             
-                            Image(systemName: detailModel.icon[day.weather.first!.icon] ?? "exclamationmark.square")
+                            Image(systemName: detailModel.icon[day.icon] ?? "exclamationmark.square")
                                 .symbolRenderingMode(.multicolor)
-                                
+                            
                             Spacer()
                             
-                            Text("\(Int(day.temp.day))°C")
-                                .frame(width: 60, alignment: .trailing)
+                            Text("\(Int(day.temp))°C")
+                                .frame(width: 80, alignment: .trailing)
                         }
                         .padding(.bottom, 3)
                         
@@ -59,6 +78,7 @@ struct DetailView: View {
             .padding(.horizontal, 20)
         }
         .navigationTitle(lokalita.name)
+        .navigationBarTitleDisplayMode(.large)
         .onAppear {
             detailModel.stiahniData(lat: lokalita.latitude, lon: lokalita.longitude)
         }
@@ -69,6 +89,5 @@ struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         let bs = Lokality(name: "Banská Štiavnica", latitude: 48.4587, longitude: 18.8931)
         DetailView(lokalita: bs)
-            .preferredColorScheme(.dark)
     }
 }
